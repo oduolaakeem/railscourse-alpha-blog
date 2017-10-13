b720329af903b43b70ec0bf045c42df6f615fab1
Sean Fitzgeald <sfitzgerald107@gmail.com>
2017-10-13 15:35:44 +0000

    Add category model and test

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,9 @@ get 'login', to: 'sessions#new'
 post 'login', to: 'sessions#create'
 delete 'logout', to: 'sessions#destroy'
 
+
+resources :categories, except: [:destroy]
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
